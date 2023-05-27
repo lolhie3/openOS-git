cfc35d888d0086a5aa1cf57a04bf4ecb1c245811
lolhie3 <96627041+lolhie3@users.noreply.github.com>
2023-05-27 23:03:37 +0400

    Update boot.lua

--- lib/core/boot.lua
+++ lib/core/boot.lua
@@ -1,7 +1,7 @@
 -- called from /init.lua
 local raw_loadfile = ...
 
-_G._OSVERSION = "OpenOS 1.7.5"
+_G._OSVERSION = "openOS-git 1.7.5"
 
 -- luacheck: globals component computer unicode _OSVERSION
 local component = component
